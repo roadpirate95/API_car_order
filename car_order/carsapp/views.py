78c492ae38b75b5cc093235e5592a1c05622fa4a
VY <vlad.yushin.95@bk.ru>
2022-10-12 13:36:29 +0300

    pagination

--- car_order/carsapp/views.py
+++ car_order/carsapp/views.py
@@ -2,7 +2,7 @@ from django.db.models import Prefetch
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from .pagination import MyLimitOffsetPagination
 from .helpers import DataOrder, SortedInfoOrder
 from .models import CarModel, CarMake, Color, Order
 from .serializers import OrderModelSerializer, ColorSerializer, CarModelSerializer, CarMakeSerializer,\
@@ -24,13 +24,22 @@ class ColorViewSet(viewsets.ModelViewSet):
     """
     queryset = Color.objects.all()
     serializer_class = ColorSerializer
+    pagination_class = MyLimitOffsetPagination
 
     @action(detail=False)
     def color_and_amount(self, request):
         color_orders_join = Color.objects.prefetch_related(Prefetch('order_color', to_attr='prefetched_order'))
-        serializer = ColorInfoSerializer(color_orders_join, many=True)
+        page = self.paginate_queryset(color_orders_join)
+        if page is not None:
+            serializer = ColorInfoSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
 
+        serializer = ColorInfoSerializer(color_orders_join, many=True)
         return Response(serializer.data)
+
+        # serializer = ColorInfoSerializer(color_orders_join, many=True)
+        #
+        # return Response(serializer.data)
 
 
 
@@ -48,15 +57,23 @@ class CarMakeViewSet(viewsets.ModelViewSet):
     """
     queryset = CarMake.objects.all()
     serializer_class = CarMakeSerializer
+    pagination_class = MyLimitOffsetPagination
 
     @action(detail=False)
     def make_and_amount(self, request):
         make_order_join = CarMake.objects.prefetch_related(
             Prefetch('car_models', queryset=CarModel.objects.prefetch_related('order_model')))
+        page = self.paginate_queryset(make_order_join)
+        if page is not None:
+            serializer = CarMakeInfoSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
 
         serializer = CarMakeInfoSerializer(make_order_join, many=True)
-
         return Response(serializer.data)
+
+        # serializer = CarMakeInfoSerializer(make_order_join, many=True)
+        #
+        # return Response(serializer.data)
 
 
 class OrderInfo(APIView):
